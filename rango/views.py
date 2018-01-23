714ad5f40d5fb79b2c1ee4c0caba2cf66f6dd6f6
Yonghong Xu <2289141x@student.gla.ac.uk>
2018-01-23 03:03:28 +0000

    add chapter 3 and 4 exercise

--- rango/views.py
+++ rango/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 def index(request):
     context_dict = {'boldmessage': "Crunchy,creamy,cookie,candy,cupcake"}
     return render(request, 'rango/index.html', context=context_dict)
+
+
+def about(request):
+    # return HttpResponse("Rango says here is the about page.")
+    return render(request, 'rango/about.html')
