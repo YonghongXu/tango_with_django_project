d124960304b666b8092f07025fefc124ab10adac
Yonghong Xu <2289141x@student.gla.ac.uk>
2018-02-01 21:46:29 +0000

    chapter 6

--- populate_rango.py
+++ populate_rango.py
@@ -10,29 +10,32 @@ from rango.models import Category, Page
 
 def populate():
     python_pages = [
-        {"title": "Official Python Tutorial", "url": "http://docs.python.org/2/tutorial/"},
-        {"title": "How to Think like a Computer Scientist", "url": "http://www.greenteapress.com/thinkpython/"},
-        {"title": "Learn Python in 10 Minutes", "url": "http://www.korokithakis.net/tutorials/python/"}
+        {"title": "Official Python Tutorial", "url": "http://docs.python.org/2/tutorial/", "views": 100},
+        {"title": "How to Think like a Computer Scientist", "url": "http://www.greenteapress.com/thinkpython/",
+         "views": 70},
+        {"title": "Learn Python in 10 Minutes", "url": "http://www.korokithakis.net/tutorials/python/", "views": 50}
     ]
 
     django_pages = [
-        {"title": "Official Django Tutorial", "url": "https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
-        {"title": "Django Rocks", "url": "http://www.djangorocks.com/"},
-        {"title": "How to Tango with Django", "url": "http://www.tangowithdjango.com/"}
+        {"title": "Official Django Tutorial", "url": "https://docs.djangoproject.com/en/1.9/intro/tutorial01/",
+         "views": 80},
+        {"title": "Django Rocks", "url": "http://www.djangorocks.com/", "views": 40},
+        {"title": "How to Tango with Django", "url": "http://www.tangowithdjango.com/", "views": 30}
     ]
 
-    other_pages = [{"title": "Bottle", "url": "http://bottlepy.org/docs/dev/"},
-                   {"title": "Flask", "url": "http://flask.pocoo.org"}]
+    other_pages = [{"title": "Bottle", "url": "http://bottlepy.org/docs/dev/", "views": 90},
+                   {"title": "Flask", "url": "http://flask.pocoo.org", "views": 60}]
 
-    cats = {"Python": {"pages": python_pages},
-            "Django": {"pages": django_pages},
-            "Other Frameworks": {"pages": other_pages}
+    cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
+            "Django": {"pages": django_pages, "views": 64, "likes": 32},
+            "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16}
             }
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        # c = add_cat(cat)
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], p["views"])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
@@ -58,6 +61,3 @@ def add_cat(name, views=0, likes=0):
 if __name__ == '__main__':
     print("Starting Rango population script...")
     populate()
-    add_cat("Python", 128, 64)
-    add_cat("Django", 64, 32)
-    add_cat("Frameworks", 32, 16)
